06e70f15077c9009f2ecec5f02f0a3ebcd4930d4
valderramagp <valderramagp@gmail.com>
2016-11-03 09:55:50 -0500

    prueba

---
+++ src/agenda/Persona.java
@@ -0,0 +1,12 @@
+package agenda;
+
+/**
+ * Created by gusta on 03/11/2016.
+ */
+public class Persona {
+    private int id;
+    private String nombre;
+    private String telefono;
+    private String correo;
+
+}
