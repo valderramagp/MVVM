bc5aee438b695e494692a8ce39b55eb851ff723d
valderramagp <valderramagp@gmail.com>
2016-11-03 12:47:00 -0500

    Views Implementation

--- src/agenda/Test/Main.java
+++ src/agenda/Test/Main.java
@@ -3,9 +3,11 @@ package agenda.Test;
 import agenda.DBConnection.DBRepo;
 import agenda.Models.Persona;
 import agenda.ViewModels.PersonaVM;
+import agenda.Views.DetallePersona;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 /**
  * Created by gusta on 03/11/2016.
@@ -13,7 +15,13 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) {
         PersonaVM vm = new PersonaVM();
-        Persona persona = vm.GetPersona(1);
-        System.out.println(persona.getNombre());
+        vm.GetPersona(1);
+
+
+        /*ArrayList<Persona> personas = vm.GetPersonas();
+        for(Persona p : personas){
+            System.out.println(p.getNombre());
+        }*/
+
     }
 }
--- src/agenda/ViewModels/PersonaVM.java
+++ src/agenda/ViewModels/PersonaVM.java
@@ -2,26 +2,49 @@ package agenda.ViewModels;
 
 import agenda.DBConnection.DBRepo;
 import agenda.Models.Persona;
+import agenda.Views.DetallePersona;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 /**
  * Created by gusta on 03/11/2016.
  */
 public class PersonaVM {
+    private DBRepo con;
 
-    public Persona GetPersona(int idPersona){
+    public PersonaVM(){
+        con = DBRepo.getConnection();
+    }
+
+    public ArrayList<Persona> GetPersonas() {
+        ArrayList<Persona> personas = new ArrayList<Persona>();
+        String sql = "SELECT * FROM persona";
         try {
-            DBRepo con = DBRepo.getConnection();
-            String sql = "SELECT * FROM persona WHERE id = " + idPersona;
             ResultSet rs = con.query(sql);
-
-            if(rs.next()){
-                return new Persona(rs.getInt("id"), rs.getString("nombre"), rs.getString("telefono"), rs.getString("correo"));
+            while (rs.next()){
+                Persona  persona = new Persona(rs.getInt("id"), rs.getString("nombre"), rs.getString("telefono"), rs.getString("correo"));
+                personas.add(persona);
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return null;
+        return personas;
+    }
+
+    public void GetPersona(int idPersona){
+        try {
+
+            String sql = "SELECT * FROM persona WHERE id = " + idPersona;
+            ResultSet rs = con.query(sql);
+
+            if(rs.next()){
+                Persona persona = new Persona(rs.getInt("id"), rs.getString("nombre"), rs.getString("telefono"), rs.getString("correo"));
+                DetallePersona dp = new DetallePersona(persona);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
