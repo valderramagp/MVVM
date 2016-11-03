8ffa3364a233a0917365e292c196005d6dda3217
valderramagp <valderramagp@gmail.com>
2016-11-03 10:02:08 -0500

    prueba2

--- src/agenda/Persona.java
+++ src/agenda/Persona.java
@@ -9,4 +9,42 @@ public class Persona {
     private String telefono;
     private String correo;
 
+    public Persona(int id, String nombre, String telefono, String correo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.correo = correo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
 }
